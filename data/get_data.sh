a9414b5177e78d986cc94e83b5e4edd29efbf364
mgabdo@ksu.edu <mgabdo@ksu.edu>
2018-11-01 15:21:05 -0500

    locating the data in inputs

--- data/get_data.sh
+++ data/get_data.sh
@@ -2,5 +2,5 @@
 read -p "Enter username: " name
 name=${name:-$USER}
 # 22x22 2-g diffusion data
-scp -P 22222 $name@eigendoit.mne.ksu.edu:/opt/data/LRA/diffusion2x2_ref_with_mesh_temps.p .
+scp -P 22222 $name@eigendoit.mne.ksu.edu:/opt/data/LRA/diffusion2x2_ref_with_mesh_temps.p ./inputs/.
 
